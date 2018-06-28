9c7838c7d0cdc7b16364a52deb3a6461d79d7b50
Victor Schirmann <victorino_s@outlook.com>
2018-06-28 14:55:10 +0200

    RawMetrics.API Done - RawMetrics.API.Tests Done

---
+++ RawMetrics.API/IRawMetricsService.cs
@@ -0,0 +1,20 @@
+﻿using Atlantis.RawMetrics.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RawMetrics.API
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IService1" à la fois dans le code et le fichier de configuration.
+    [ServiceContract]
+    public interface IRawMetricsService
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "/getRawMetricsFromDevice?deviceId={deviceId}&date={date}&amount={amount}", ResponseFormat = WebMessageFormat.Json)]
+        List<RawMetric> GetRawMetricsFromDevice(string deviceId, long date, int amount);
+    }
+}
---
+++ RawMetrics.API/RawMetricsService.svc.cs
@@ -0,0 +1,41 @@
+﻿using Atlantis.RawMetrics.DAL;
+using Atlantis.RawMetrics.DAL.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RawMetrics.API
+{
+    public class RawMetricsService : IRawMetricsService
+    {
+        private RawMetricsDAO _dao;
+
+        public RawMetricsService()
+        {
+            var _context = new RawMetricsContext(true);
+            _dao = new RawMetricsDAO(_context);
+        }
+
+        public RawMetricsService(RawMetricsDAO dao)
+        {
+            _dao = dao;
+        }
+
+        public List<RawMetric> GetRawMetricsFromDevice(string deviceId, long date, int amount)
+        {
+            if (deviceId == null || deviceId.Length == 0)
+                throw new System.ServiceModel.Web.WebFaultException<string>("DeviceId cannot be empty.", System.Net.HttpStatusCode.BadRequest);
+            if (amount > 0)
+            {
+                var results = _dao.GetMetricsForDevice(deviceId).Where(m => m.Date < new DateTime(date).Ticks).Take(amount);
+                return results.ToList();
+            }
+            return new List<RawMetric>();
+        }
+    }
+}
